2476a7d47c6cfdea1fc746c66636fbd509f5c70c
fuushyn <110640872+fuushyn@users.noreply.github.com>
2023-03-07 10:25:12 +0530

    added variable

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from langchain.llms import OpenAI
 
 def load_chain():
     """Logic for loading the chain you want to use should go here."""
+    openai_api_key = st.secrets["OPENAI_API_KEY"]
     llm = OpenAI(temperature=0)
     chain = ConversationChain(llm=llm)
     return chain
